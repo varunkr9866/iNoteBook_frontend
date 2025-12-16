cb15b61a45875be40a87f3b1dff68f39bf5ae923
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-16 11:39:32 +0530

    COMPLETED BACKEND CONNECTION

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+        .then(() => console.log("Connected to Mongo Successfully"))
+        .catch(err => console.error("Mongo connection error:", err));
+};
+
+module.exports = connectToMongo;
