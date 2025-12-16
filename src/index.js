87a34a1950cb9e6cbbe4785491483fe3cc454ed6
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-16 11:57:33 +0530

    WORKING ON CSS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import {
-  BrowserRouter
-} from "react-router-dom"; 
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
- 
