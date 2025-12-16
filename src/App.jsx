c4da8796eb61840b110d230e6e9a10bc357d4ad8
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-16 11:51:55 +0530

    FIXING AN ERROR

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
+import { Alert } from "./components/Alert";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div >
-    
-    </div>
+    <>
+      <NoteState>
+        <Navbar />
+        <Alert message="This is amazing React course" />
+
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </NoteState>
+    </>
   );
 }
 
